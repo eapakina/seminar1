7eb5d4f748fd3dd4ebcced37aed353dff5cd6ab1
E.Apakina <e.apakina@gmail.com>
2022-08-05 15:58:36 +0300

    task 7

---
+++ task7/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+
+Console.WriteLine("Введите трехзначное число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100  || number > 999)
+{
+    Console.WriteLine("Неверное число");}
+
+
+
+while (number>100)
+{
+number = number - 100;}
+
+while (number>10)
+{
+number = number - 10;}
+
+Console.WriteLine(number);
