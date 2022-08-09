a9b30353a57b1f236779d9ddbf9b32276f6947d1
E.Apakina <e.apakina@gmail.com>
2022-08-09 21:13:47 +0300

    task9

---
+++ task9/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 9. Программа выводящее случайное число из отрезка 10-99 и показывает наибольшую цифру
+
+int number = new Random().Next(10,100);
+int digit2 = number % 10;
+int digit1 = number / 10;
+Console.WriteLine(number);
+if (digit1 > digit2)
+{
+    Console.WriteLine(digit1);
+
+}
+else 
+{
+if (digit1 == digit2)
+{
+    Console.WriteLine("цифры равны");
+
+}
+
+else
+{
+    Console.WriteLine(digit2);
+}}
