2a93a748904479a1461ce54d1f8d1b518d3f0747
E.Apakina <e.apakina@gmail.com>
2022-08-17 15:51:04 +0300

    task28

---
+++ task28/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("введите число A");
+int A =Convert.ToInt32(Console.ReadLine());
+
+int sum = 1;
+
+for (int i = 1; i <= A; i++)
+{
+    sum = sum * i;
+}
+
+Console.WriteLine ($"Произведение чисел от 1 до А = {sum}");
