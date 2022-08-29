2c0ea11fb697f9ab647d32c7475a7c30372d0a90
E.Apakina <e.apakina@gmail.com>
2022-08-29 22:47:53 +0300

    task56

---
+++ task56/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив.
+//  Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = -10, int rightRange = 10)
+ {
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i =0; i < matrix.GetLength(0); i++)
+    {
+        for (int j =0; j < matrix.GetLength(1); j++)
+        {
+        matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+ }
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("введите m "); //число строк
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("введите n "); //число строк
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = FillMatrix(m,n);
+PrintMatrix(matrix);
+
+int[] array = new int[m];
+int sum = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum = sum + matrix[i,j];
+        }
+       array [i] = sum;
+       sum = 0;
+    }
+
+
+Console.WriteLine();
+    Console.WriteLine(string.Join(", ", array));
+
+int min = array[0];
+int imin = 0;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+      if (array[i] < min)
+      {
+        min = array[i];
+        imin = i;
+      }
+    }
+
+        Console.WriteLine($"номер строки с наименьше суммой элементов равен {imin + 1}"); //математический номер строки
