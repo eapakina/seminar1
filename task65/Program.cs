2d728f1914f72aa7c441ea5b481a269b11555fe7
E.Apakina <e.apakina@gmail.com>
2022-09-02 16:22:53 +0300

    task66

---
+++ task65/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+Console.WriteLine("введите M ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите N ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+PrintNumbersNToM(numberM, numberN);
+
+void PrintNumbersNToM(int m, int n)
+{
+    if (n < m) return;
+    Console.Write(m + " ");
+    PrintNumbersNToM(m+1, n);
+    }
---
+++ task66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("введите M ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите N ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM > numberN)
+{
+int Sum = Summa(numberN, numberM);
+Console.WriteLine(Sum);
+
+}
+else
+{
+int Sum = Summa(numberM, numberN);
+Console.WriteLine(Sum);
+
+}
+
+int Summa(int number1, int number2)
+{
+    if ((number2 - number1) <=1 ) return number1 + number2;
+     return (number1 + Summa (number1 + 1, number2));
+}
+
+    
